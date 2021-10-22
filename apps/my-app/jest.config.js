0dfd942e734a4d5871a57814e86fe5bc88465b7d
Brandon Roberts <brandon@nrwl.io>
2021-10-22 10:55:14 -0500

    initial commit

---
+++ apps/my-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'my-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/my-app',
+};
